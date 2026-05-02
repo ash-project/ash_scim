e1c02e4dc53c7876a062b13b0a3b3a03e41f92d7
Zach Daniel <zach@zachdaniel.dev>
2026-05-02 12:10:57 -0400

    🔥 initial commit 🔥

---
+++ lib/ash_scim.ex
@@ -0,0 +1,18 @@
+defmodule AshScim do
+  @moduledoc """
+  Documentation for `AshScim`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AshScim.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ash_scim_test.exs
@@ -0,0 +1,8 @@
+defmodule AshScimTest do
+  use ExUnit.Case
+  doctest AshScim
+
+  test "greets the world" do
+    assert AshScim.hello() == :world
+  end
+end
